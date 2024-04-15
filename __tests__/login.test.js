cdae31a2df38a532bdc5e7b4976728af0b403cc3
FlazyFlad <igorpak1337@gmail.com>
2024-04-15 17:53:33 +0500

    travis CI tests

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -10,18 +10,18 @@ describe('POST /login', () => {
       .post('/users/login')
       .send({ email: 'john@gmail.com', password: '123456' });
 
-    expect(res.status).to.equal(200);
+    expect(res.statusCode).to.equal(200);
     expect(res.body).to.have.property('accessToken');
     expect(res.body).to.have.property('refreshToken');
-  });
+  }).timeout(10000);
 
   it('should return 401 with incorrect credentials', async () => {
     const res = await request(app)
       .post('/users/login')
       .send({ email: 'test@example.com', password: 'wrongpassword' });
 
-    expect(res.status).to.equal(401);
-    expect(res.body).to.have.property('message', 'User not found');
+    expect(res.statusCode).to.equal(401);
+    expect(res.body).to.have.property('message', 'Пользователь не найден');
   });
 
   it('should return 401 with incorrect password', async () => {
@@ -29,16 +29,16 @@ describe('POST /login', () => {
       .post('/users/login')
       .send({ email: 'john@gmail.com', password: 'wrongpassword' });
 
-    expect(res.status).to.equal(401);
-    expect(res.body).to.have.property('message', 'Incorrect password');
+    expect(res.statusCode).to.equal(401);
+    expect(res.body).to.have.property('message', 'Пароль не верный');
   });
 
-  it('should return 400 with possible SQL injection attempt', async () => {
+  it('should return 400 with possible sql injection', async () => {
     const res = await request(app)
       .post('/users/login')
       .send({ email: 'user2@gmail.com', password: '$select * from$' });
 
-    expect(res.status).to.equal(400);
-    expect(res.body).to.have.property('message', 'Password contains dangerous characters (possible SQL injection)');
+    expect(res.statusCode).to.equal(400);
+    expect(res.body).to.have.property('message', 'В пароле содержится опасные символы (возможная SQL-инъекция)');
   });
 });
